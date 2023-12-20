e0bed84d96a1f6949df317883b5dfc618f021454
stdjes <133016938+stdjes@users.noreply.github.com>
2023-12-20 14:36:25 -0600

    Update test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -5,15 +5,15 @@ from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """doc doc"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """doc"""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """doc doc"""
         new = self.value()
         self.assertEqual(type(new.name), str)
