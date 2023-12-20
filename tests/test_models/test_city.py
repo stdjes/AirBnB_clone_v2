3d1968a656202d0fc1d417a11cfeb102cc9d1ef0
stdjes <133016938+stdjes@users.noreply.github.com>
2023-12-20 14:38:25 -0600

    Update test_city.py

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -5,20 +5,20 @@ from models.city import City
 
 
 class test_City(test_basemodel):
-    """ """
+    """doc doc"""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """doc doc"""
         super().__init__(*args, **kwargs)
         self.name = "City"
         self.value = City
 
     def test_state_id(self):
-        """ """
+        """doc doc"""
         new = self.value()
         self.assertEqual(type(new.state_id), str)
 
     def test_name(self):
-        """ """
+        """doc doc"""
         new = self.value()
         self.assertEqual(type(new.name), str)
