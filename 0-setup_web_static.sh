5d6147181f36a239e97ccd8a331602a14efa0552
stdjes <133016938+stdjes@users.noreply.github.com>
2024-02-04 15:58:15 -0800

    fix bugs

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,48 @@
 #!/usr/bin/env bash
-# Bash script that sets up web servers for the deployment of web_static
-sudo apt-get update
-sudo apt-get -y install nginx
-sudo ufw allow 'Nginx HTTP'
+# sets up your web servers for the deployment of web_static
 
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+apt-get update -y
+apt-get install nginx -y
+ufw allow 'Nginx HTTP'
 
-sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+touch /data/web_static/releases/test/index.html
+echo "<html>
+    <head>
+    </head>
+    <body>
+        <h2>Hello From ~AirBnB~</h2>
+    </body>
+</html>" > /data/web_static/releases/test/index.html
 
-sudo chown -R ubuntu:ubuntu /data/
+ln -sf /data/web_static/releases/test/ /data/web_static/current
+chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+CONFIGs=\
+"server {
+    listen 80 default_server;
 
-sudo service nginx restart
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+
+    root /etc/nginx/html;
+    index index.html index.htm;
+
+    location /redirect_me {
+        return 301 https://github.com/Y-Baker;
+    }
+
+    error_page 404 /error_404.html;
+    location /404 {
+        root /etc/nginx/html;
+        internal;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+}"
+bash -c "echo -e '$CONFIGs' > /etc/nginx/sites-available/default"
+
+service nginx restart
